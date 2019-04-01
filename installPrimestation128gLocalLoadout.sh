d8c0ce900948e747d5e29af105aacb50d318d54a
Chris Paiano <chris.paiano@gmail.com>
2019-04-01 16:02:13 -0700

    First pass at creating a usb stick loadout script for primestation games

---
+++ installPrimestation128gLocalLoadout.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+pushd /Volumes/PRIMEGAMES/retropie-mount
+
+#megaInstallPsxPocketFighter.sh
+unmegalocal.sh /Volumes/MacBak001/primestationoneMegaFiles/megaPsxPocketFighter.tar.bz2
+
+#megaInstallBinsNRomsLarge.sh
+unmegalocal.sh /Volumes/MacBak001/primestationoneMegaFiles/binsnroms_large.tar.bz2
+
+
+# megaInstallNesOrganizedRoms.sh
+unmegalocal.sh /Volumes/MacBak001/primestationoneMegaFiles/megaNesOrganizedRoms.tar.bz2
+
+# megaInstallSnesRoms.sh
+unmegalocal.sh /Volumes/MacBak001/primestationoneMegaFiles/megaSnesRoms.tar.bz2
+
+
+megaInstallStarcraftWarcraftStratagusRoms.sh
+megaInstallAtari5200Roms.sh
+megaInstallAtariLynxRoms.sh
+megaInstallC64Roms.sh
+megaInstallGameGearRoms.sh
+megaInstallGbaRoms.sh
+megaInstallGbcRoms.sh
+megaInstallGbRoms.sh
+megaInstallGenesisRoms.sh
+megaInstallIntellivisionRoms.sh
+megaInstallMasterSystemRoms.sh
+megaInstallSnesMouseRoms.sh
+megaInstallTg16Roms.sh
+megaInstallVectrexRoms.sh
+megaInstallDcMarvelVsCapcom1.sh
+megaInstallDcMarvelVsCapcom2.sh
+megaInstallDcPowerStone.sh
+megaInstallDcSanFranciscoRush2049.sh
+megaInstallPsxBushidoBlade1.sh
+megaInstallPsxDeception3DarkDelusion.sh
+megaInstallPsxFrogger.sh
+megaInstallPsxGranTurismo1.sh
+megaInstallPsxGranTurismo2ArcadeDisc.sh
+megaInstallPsxGranTurismo2SimulationDisc.sh
+megaInstallPsxMetalGearSolid.sh
+megaInstallPsxMetalSlugX.sh
+megaInstallPsxPepsiman.sh
+megaInstallPsxTecmosDeception1.sh
+megaInstallPsxTekken3.sh
+megaInstallDcSoulCalibur.sh
+megaInstallScdNightTrap.sh
+megaInstallPsxFinalFantasy7.sh
+# megaInstallDcGauntletLegends.sh
+megaInstallDcDeadOrAlive2.sh
+megaInstallDcVirtuaFighter3TB.sh
+megaInstallDcResidentEvil3Nemesis.sh
+# megaInstallDcZombieRevenge.sh
+megaInstallApple2Roms.sh
+megaInstallDoomWads.sh
+megaInstallNintendoDsSelectRoms.sh
+megaInstallPsxAustinPowersPinball.sh
+megaInstallPsxColinMcraeRallyV2_1Hacked.sh
+megaInstallPsxCoolBoarders3.sh
+megaInstallPsxInitialD.sh
+megaInstallPsxJackieChanStuntmaster.sh
+megaInstallPsxJadeCocoon.sh
+megaInstallPsxMtvMusicGenerator.sh
+megaInstallPsxUmJammerLammy.sh
+megaInstallPsxVigilante82ndOffense.sh
+megaInstallScdNightTrap.sh
+megaInstallSvm7thGuest.sh
+megaInstallVirtualBoyRoms.sh
+megaInstallWonderswanColorRoms.sh
+megaInstallWonderswanRoms.sh
+# megaInstallDcPhantasyStarOnline.sh
+megaInstallMame2000v037b5Roms.sh
+megaInstallFbaRoms.sh
+# megaInstallN64Roms.sh
+megaInstallMame2003v078Roms.sh
+
+
+popd
