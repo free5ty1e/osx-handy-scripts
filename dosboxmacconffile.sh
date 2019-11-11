4aa1eaf64f4038ead68c340d5cdc86bda8b344a6
Chris Paiano <chris.paiano@gmail.com>
2019-11-11 00:42:08 -0800

    Added default dosbox.conf filename to script

--- dosboxmacconffile.sh
+++ dosboxmacconffile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-/Applications/dosbox.app/Contents/MacOS/DOSBox --conf "%1"
+#PARAMETER 1: CONF_FILE optional output folder if we want to decompress to somewhere else
+if [ -z "$1" ]; then
+    echo "parameter 1: CONF_FILE is optional!  Defaulting to current folder..."
+    CONF_FILE="dosbox.conf"    
+else
+	CONF_FILE="$1"
+fi
+
+/Applications/dosbox.app/Contents/MacOS/DOSBox --conf "$CONF_FILE"
 
