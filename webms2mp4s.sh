676f9d2432cf3db5f54ab195ad5c798690c98e66
Chris Paiano <chris.paiano@gmail.com>
2019-11-14 12:51:22 -0800

    webm transcode script drop to h.264 instead of 265 as it's just not supported in enough places yet

--- webms2mp4s.sh
+++ webms2mp4s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -name "*.webm" -execdir HandBrakeCLI --preset "Matroska/H.265 MKV 1080p30" --encoder-preset "veryslow" --two-pass --rate "60.0" --vfr --input "{}" --output "{}.mp4" \; -exec /bin/rm "{}" \;
+find . -name "*.webm" -execdir HandBrakeCLI --preset "Matroska/H.264 MKV 1080p30" --encoder-preset "veryslow" --two-pass --rate "60.0" --vfr --input "{}" --output "{}.mp4" \; -exec /bin/rm "{}" \;
  
 
