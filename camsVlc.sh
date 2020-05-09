747b41796dc3ba42a5f8ef65c7d5fdb23427088f
Chris Paiano <chris.paiano@gmail.com>
2020-05-09 14:45:30 -0700

    Created new camsVlc.sh script to auto launch all webcams in separate VLC instances until I get the VLM mosaic thing figured out

---
+++ camsVlc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
+{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
+
