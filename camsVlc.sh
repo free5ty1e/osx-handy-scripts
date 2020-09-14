5b8e97f5d6c492f930922d7917ac14a2cd6f04f2
Chris Paiano <chris.paiano@gmail.com>
2020-09-13 22:33:47 -0700

    Added ruckuscam and drivewaycam

--- camsVlc.sh
+++ camsVlc.sh
@@ -2,4 +2,5 @@
 
 { /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
 { /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
-
+{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/DrivewayCam.vlc & } 2>/dev/null
+{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/RuckusCam.vlc & } 2>/dev/null
