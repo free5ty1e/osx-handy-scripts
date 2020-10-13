8916fbb6039f4a62ea048475ee653d3d0058c0a9
Chris Paiano <chris.paiano@gmail.com>
2020-10-12 21:28:40 -0700

    7zeachfilerecursively

---
+++ 7zEachFileRecursively.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Finding each file in current folder structure and 7zipping individually"
+
+#for file in "$1"; do
+#filename=$(basename {})
+#    extension="${filename##*.}"
+#    headerfilename="${filename%.*}"
+#    filenameOnly="${headerfilename%.*}"
+#    zip -9 "$filenameOnly.zip" "$file"
+#done
+
+#find . -name "*$1*" -print -exec zip -9 "${'{}'##*/}" '{}' \;
+
+find . -type f -execdir compress7z '{}' \;
