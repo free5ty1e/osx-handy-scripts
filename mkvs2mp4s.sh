398bcc5d62dfdcd7787331a8e643863d267227c6
Chris Paiano <chris.paiano@gmail.com>
2020-11-13 13:45:32 -0800

    mkvs2mp4s.sh

---
+++ mkvs2mp4s.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find . -name "*.mkv" -execdir HandBrakeCLI --preset "Matroska/H.264 MKV 1080p30" --encoder-preset "veryslow" --two-pass --rate "60.0" --vfr --input "{}" --output "{}.mp4" \; -exec /bin/rm "{}" \;
+ 
