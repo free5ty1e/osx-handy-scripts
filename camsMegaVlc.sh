48169f8b4a04392c9ebfe066158020a75deb5dc1
Chris Paiano <chris.paiano@gmail.com>
2021-02-25 19:36:20 -0800

    cams scripts now disable audio track by default and autozoom to specified level

--- camsMegaVlc.sh
+++ camsMegaVlc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGA/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGA/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGA/MEGAsync/Stream/DrivewayCam.vlc & } 2>/dev/null
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGA/MEGAsync/Stream/RuckusCam.vlc & } 2>/dev/null
+ZOOM=0.46
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGA/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGA/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGA/MEGAsync/Stream/DrivewayCam.vlc & } 2>/dev/null
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGA/MEGAsync/Stream/RuckusCam.vlc & } 2>/dev/null
--- camsVlc.sh
+++ camsVlc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/DrivewayCam.vlc & } 2>/dev/null
-{ /Applications/VLC.app/Contents/MacOS/VLC ~/MEGAsync/Stream/RuckusCam.vlc & } 2>/dev/null
+ZOOM=0.46
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGAsync/Stream/Sphene.vlc & } 2>/dev/null 
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGAsync/Stream/Rupee.vlc & } 2>/dev/null
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGAsync/Stream/DrivewayCam.vlc & } 2>/dev/null
+{ /Applications/VLC.app/Contents/MacOS/VLC --no-audio --zoom $ZOOM ~/MEGAsync/Stream/RuckusCam.vlc & } 2>/dev/null
