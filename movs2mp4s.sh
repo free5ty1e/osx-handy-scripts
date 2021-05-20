a36ffbe0c57c1815c973248903a851ffba7b23f3
Chris Paiano <chris.paiano@gmail.com>
2021-05-20 09:26:43 -0700

    Created movs2mp4s.sh

---
+++ movs2mp4s.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find . -name "*.mov" -execdir HandBrakeCLI --preset "Matroska/H.264 MKV 1080p30" --encoder-preset "veryslow" --two-pass --rate "60.0" --vfr --input "{}" --output "{}.mp4" \; -exec /bin/rm "{}" \;
